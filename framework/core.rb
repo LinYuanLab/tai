165924c8b78565475bf806a479f11f0e2d5a9498
Chorder <chorder@chorder.net>
2016-02-18 15:40:34 +0800

    2016-02-18

--- framework/core.rb
+++ framework/core.rb
@@ -19,36 +19,62 @@ def args_parse(options)
 	end
 	if import = options[:import]
 		puts "Launch Task:#{import}"
+		#执行导入的扫描任务
+		create_task( 3, "task_path", targets )
+		
 	elsif export = options[:export]
-		puts "Export Task:#{export}" #导出扫描任务
+		#导出扫描任务
+		puts "Export Task:#{export}" 
 	elsif list = options[:list]
-		puts "list: #{list}"	#显示对象
+		#显示对象
+		puts "list: #{list}"
 	elsif options[:update]
-		puts "updating..."	#更新规则库
+		#更新规则库
+		puts "updating..."
 	elsif options[:init]
-		puts "initialize"	#初始化本地数据库
+		#初始化本地数据库
+		puts "initialize"
 	else
 		if target = options[:target]
-			target = parse_target( target )
+			#这里使用函数对目标进行处理，输出数组格式的任务目标
+			targets = parse_target( target )
+			
 			if rule = options[:rule]
+				#如果指定了特定的规则，这里将使用特定的脚本进行扫描
 				puts "Load Rule: #{rule}"
+				#创建特定规则任务
+				create_task( 2, "rule_name", targets )
 			else
 				if script = options[:script]
+					#如果指定了单个脚本进行批量扫描，那么就从这里启动扫描
 					puts "Use Script: #{script}"
+					create_task( 1, "script_name", targets )
 				else
+					#不然的话就启动默认规则，从这里进行扫描
 					puts "Load Default Rule"
+					#默认的规则是启用全部的脚本
+					create_task( 0, 0, targets )
 				end
 			end
-			
-			#处理完参数，将参数传入扫描进程启动扫描
-			
 		else
+			#没有指定扫描目标，程序退出
 			puts "Target Cannot Be Empty!\n#{Time.now.strftime('%Y/%m/%d %H:%M:%S')}"
 		end
 	end
 end
 
+#为了推进后面的程序流程临时编写的替代函数
 def parse_target( item )
+	targets = Array.new
+	targets.push( item )
+end
+
+#这个才是真正的目标处理函数
+#这个函数仅仅把目标处理成数组
+#0.如果是单个ip或域名，直接返回成数组
+#1.如果是域名类型，类似a.com和a.com,b.com或a.com b.com,自动分割组建数组
+#2.如果是网络地址类型，切换成单个ip后组成数组，如192.168.0.0/24，返回的数组内容为192.168.0.1-192.168.0.255
+def parse_target_( item )
 	reg_domain = /[\w\d\-_\.]+[a-z]{2,4}/n
 	reg_ipaddr = /[[0-9]{1,3}\.]{3}\.[0-9]{1,3}/n
 	reg_iprang = /[[0-9]{1,3}\.]{3}\.[0-9]{1,3}\/[0-9]{1,2}/n
@@ -65,8 +91,39 @@ def parse_target( item )
 	return item
 end
 
-def creat_task()
-	
+#创建扫描任务
+def create_task(type, item, targets)
+=begin
+该函数参数为：
+type:任务类型
+	0:默认类型
+	1:single scripts
+	2:define rule
+	3:launch task
+item:参数
+	type	item		targets
+	0		0			targets
+	1		script_name	targets
+	2		rule_name	targets
+	3		task_path	null
+targets:目标，数组类型
+=end
+	case type
+		when 0
+			puts "default task"
+			
+		when 1
+			puts "script task"
+		when 2
+			puts "rule task"
+		when 3
+			puts "launch task"
+		else
+			puts "unknow task"
+		end
+	targets.each do | target |
+		puts target
+	end
 end
 
 #检查版本和规则更新
